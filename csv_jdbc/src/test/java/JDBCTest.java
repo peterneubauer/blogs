63047ddeafde84396f23652060f3a4a0e4e2106b
Peter Neubauer <peter@neubauer.se>
2014-05-30 14:33:51 +0200

    starting on the blog

--- csv_jdbc/src/test/java/JDBCTest.java
+++ csv_jdbc/src/test/java/JDBCTest.java
@@ -1,31 +1,41 @@
 import org.junit.Test;
+import org.neo4j.helpers.collection.MapUtil;
 
 import java.sql.*;
+import java.util.Map;
 
 public class JDBCTest {
 
     @Test
     public void loadCSV() throws Exception {
 
+        //load the Neo4j JDBC driver class
         Class.forName("org.neo4j.jdbc.Driver");
 
+        //connect to a vanilla default local neo4j server
         Connection con = DriverManager.getConnection("jdbc:neo4j://localhost:7474/");
 
-        try (Statement stmt = con.createStatement()) {
+        try {
+            Statement stmt = con.createStatement();
             //delete all existing content
             stmt.execute("match (n) optional match (n)-[r]-() delete n, r");
 
+            //the property map for the "neo4j" node
+            final Map<String, String> neo4jProperties = MapUtil.stringMap("name", "neo4j");
+
             //insert some one-time data
-            stmt.execute("create (:Project{name:'neo4j'})");
+            final PreparedStatement insertNeo4j = con.prepareStatement("create (:Project {1})");
+            insertNeo4j.setObject(1, neo4jProperties);
+            insertNeo4j.execute();
 
-            //some repetitive query as a prepared statement
+            //some repetitive query as a prepared statement reusing the neo4j values in the parameters map
             final PreparedStatement insertPeople = con.prepareStatement("" +
-                    "MATCH (neo4j:Project{name:'neo4j'}) " +
+                    "MATCH (neo4j:Project{name:{0}.name}) " +
                     "CREATE ({name:{1}})-[:CONTRIBUTES_TO]->(neo4j)");
-
+            insertPeople.setObject(0, neo4jProperties);
             //some bulk-data, possibly read from files or other data sources
             String[] names = {"Michael", "Emil"};
-            for(String name : names) {
+            for (String name : names) {
                 insertPeople.setString(1, name);
                 insertPeople.execute();
             }
@@ -35,6 +45,8 @@ public class JDBCTest {
             while (rs.next()) {
                 System.out.println(rs.getString("name"));
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
