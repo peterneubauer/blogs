40a1324b133fd041b2305213291719be3d07a878
Peter Neubauer <peter@neubauer.se>
2014-05-30 14:06:59 +0200

    working JDBC code

--- csv_jdbc/src/test/java/JDBCTest.java
+++ csv_jdbc/src/test/java/JDBCTest.java
@@ -1,9 +1,6 @@
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBCTest {
 
@@ -15,11 +12,28 @@ public class JDBCTest {
         Connection con = DriverManager.getConnection("jdbc:neo4j://localhost:7474/");
 
         try (Statement stmt = con.createStatement()) {
-            //delete all content
+            //delete all existing content
             stmt.execute("match (n) optional match (n)-[r]-() delete n, r");
-            ResultSet rs = stmt.executeQuery("match (n) return count(*) as count");
+
+            //insert some one-time data
+            stmt.execute("create (:Project{name:'neo4j'})");
+
+            //some repetitive query as a prepared statement
+            final PreparedStatement insertPeople = con.prepareStatement("" +
+                    "MATCH (neo4j:Project{name:'neo4j'}) " +
+                    "CREATE ({name:{1}})-[:CONTRIBUTES_TO]->(neo4j)");
+
+            //some bulk-data, possibly read from files or other data sources
+            String[] names = {"Michael", "Emil"};
+            for(String name : names) {
+                insertPeople.setString(1, name);
+                insertPeople.execute();
+            }
+
+            //some read query
+            ResultSet rs = stmt.executeQuery("match (n)-[:CONTRIBUTES_TO]->(:Project) return n.name as name");
             while (rs.next()) {
-                System.out.println(rs.getString("count"));
+                System.out.println(rs.getString("name"));
             }
         }
     }
